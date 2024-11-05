6a737847e5708d4e99ac38c24e1dc7cc2fb376f6
LinRey231175 <mr.predatorc@mail.ru>
2024-11-05 16:20:19 +0300

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        try {
+            ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+            HelloWorld helloWorldBean = applicationContext.getBean("helloworld", HelloWorld.class);
+            HelloWorld anotherHelloWorldBean = applicationContext.getBean("helloworld", HelloWorld.class);
+            Cat catBean = applicationContext.getBean("catBean", Cat.class);
+            Cat anotherCatBean = applicationContext.getBean("catBean", Cat.class);
+            System.out.println(helloWorldBean.getMessage());
+            System.out.println("Бины HelloWorld равны? " + helloWorldBean.equals(anotherHelloWorldBean));
+            System.out.println("Бины Cat равны? " + catBean.equals(anotherCatBean));
+        } catch (Exception e) {
+            System.err.println("Произошла ошибка : " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
